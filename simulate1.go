8a5344e85d5ae15a67a34d025a56c56caae16887
jayashabarishankar <89851531+jayashabarishankar@users.noreply.github.com>
2023-12-14 10:16:19 -0700

    Add files via upload

--- simulate1.go
+++ simulate1.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/vertgenlab/gonomics/dna"
 	"github.com/vertgenlab/gonomics/fasta"
+	"gonum.org/v1/gonum/mat"
 	"math/rand"
 )
 
@@ -34,11 +35,67 @@ func substitute(inBase dna.Base, mutationMatrix [][]float64) dna.Base {
 	return dna.T
 }
 
-func main() {
-	var mySeq fasta.Fasta = fasta.Fasta{Name: "mySeq", Seq: dna.StringToBases("GG--CATCCGTnnnnCACaaACCggtAT-G")}
-	var mutMatrix [][]float64 = [][]float64{[]float64{0, 1, 0, 0}, []float64{0, 1, 0, 0}, []float64{0, 1, 0, 0}, []float64{0, 1, 0, 0}}
-	fmt.Println(dna.BasesToString(mySeq.Seq))
-	answer := SimulateEvolutionFourByFour(mutMatrix, mySeq)
-	fmt.Println(dna.BasesToString(answer.Seq))
+type SubstitutionTree struct {
+	NodeName       string
+	BranchLength   float64     //this is the evolutionary time distance between this node and Up
+	MutationMatrix [][]float64 //this is the substitution probability matrix between this node and Up
+	Left           *SubstitutionTree
+	Right          *SubstitutionTree
+	Up             *SubstitutionTree
+}
 
+func BuildSubstitutionTree(startingMatrix [][]float64, startingBranchLength float64, root *SubstitutionTree) {
+	//var matrixExp float64 = root.Left.BranchLength / startingBranchLength
+
+}
+
+func floatMatrixToDenseMatrix(inMat [][]float64) *mat.Dense {
+	rows := len(inMat)
+	if rows < 1 {
+		return mat.NewDense(0, 0, nil)
+	}
+	cols := len(inMat[0])
+	dense := mat.NewDense(rows, cols, nil)
+	for currRow := 0; currRow < rows; currRow++ {
+		for currCol := 0; currCol < cols; currCol++ {
+			dense.Set(currRow, currCol, inMat[currRow][currCol])
+		}
+	}
+	return dense
+}
+func denseMatrixToFloatMatrix(dense *mat.Dense) [][]float64 {
+	rows, cols := dense.Dims()
+	var currRow, currCol int
+
+	// Create a new float matrix with the same dimensions as the dense matrix
+	floatMat := make([][]float64, rows)
+	for i := range floatMat {
+		floatMat[i] = make([]float64, cols)
+	}
+
+	// Populate the float matrix with values from the dense matrix
+	for currRow = 0; currRow < rows; currRow++ {
+		for currCol = 0; currCol < cols; currCol++ {
+			floatMat[currRow][currCol] = dense.At(currRow, currCol)
+		}
+	}
+
+	return floatMat
+}
+func main() {
+	//var mySeq fasta.Fasta = fasta.Fasta{Name: "mySeq", Seq: dna.StringToBases("GG--CATCCGTnnnnCACaaACCggtAT-G")}
+	var mutMatrix [][]float64 = [][]float64{[]float64{0.97, 0.01, 0.01, 0.01}, []float64{0.01, 0.97, 0.01, 0.01}, []float64{0.01, 0.01, 0.97, 0.01}, []float64{0.01, 0.01, 0.01, .97}}
+	//fmt.Println(dna.BasesToString(mySeq.Seq))
+	//answer := SimulateEvolutionFourByFour(mutMatrix, mySeq)
+	//fmt.Println(dna.BasesToString(answer.Seq))
+	myMat := floatMatrixToDenseMatrix(mutMatrix)
+	myMat.Scale(2, myMat)
+	myMat.Exp(myMat)
+	//fmt.Println(mat.Formatted(myMat))
+	myVec := floatMatrixToDenseMatrix([][]float64{{1}, {0}, {0}, {0}})
+	var myAnswer mat.Dense
+	myAnswer.Mul(myMat, myVec)
+	fmt.Println(denseMatrixToFloatMatrix(&myAnswer))
+	//myMat1 := denseMatrixToFloatMatrix(myMat)
+	//fmt.Println(myMat1)
 }
